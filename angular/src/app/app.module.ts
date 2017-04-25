5ecb25676c0d1f492386f1905829a45f94ac47ae
Sergei <3Hopkins@mail.ru>
2017-04-25 23:09:46 +0300

    added: миню верхнее - top-menu хлебные крошки - bread-crumbs виды - controls Refine Search - left-menu список товаров - list

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -5,10 +5,24 @@ import { HttpModule } from '@angular/http';
 import { AlertModule } from 'ng2-bootstrap/ng2-bootstrap';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { TopMenuComponent } from './top-menu/top-menu.component';
+import { BreadCrumbsComponent } from './bread-crumbs/bread-crumbs.component';
+import { ControlsComponent } from './controls/controls.component';
+import { LeftMenuComponent } from './left-menu/left-menu.component';
+import { ListComponent } from './list/list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    TopMenuComponent,
+    BreadCrumbsComponent,
+    ControlsComponent,
+    LeftMenuComponent,
+    ListComponent
   ],
   imports: [
     BrowserModule,
---
+++ angular/src/app/bread-crumbs/bread-crumbs.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-bread-crumbs',
+    templateUrl: './bread-crumbs.component.html',
+    styleUrls: ['./bread-crumbs.component.css']
+})
+export class BreadCrumbsComponent implements OnInit {
+
+    constructor() { }
+    
+    ngOnInit() {
+    }
+
+}
