ff16ab9754307af681919dddf739af5820f8d761
Sergei <3Hopkins@mail.ru>
2017-04-26 15:49:44 +0300

    create migration create_goods (name->unique())

---
+++ database/migrations/2017_04_26_124559_create_goods.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateGoods extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('goods', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name')->unique();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('goods');
+    }
+}
