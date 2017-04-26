87c8b9197565e8ca6e0ed14210796239c1d819c4
Sergei <3Hopkins@mail.ru>
2017-04-26 20:25:30 +0300

    update 2017_04_26_124559_create_goods plus column "user_id" plus column "description" plus column "text" plus column "title" plus column "video_link" plus column "map" plus column "price" plus column "old_price"

--- database/migrations/2017_04_26_124559_create_goods.php
+++ database/migrations/2017_04_26_124559_create_goods.php
@@ -16,6 +16,19 @@ class CreateGoods extends Migration
         Schema::create('goods', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name')->unique();
+            $table->integer('catalog_id')->unsigned()->index();
+//            $table->foreign('catalog_id')->references('id')->on('catalog')->onDelete('cascade');
+//            $table->foreign('catalog_id')->references('id')->on('catalog')->onUpdate('cascade');
+            $table->integer('user_id')->unsigned()->index();
+//            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+//            $table->foreign('user_id')->references('id')->on('users')->onUpdate('cascade');
+            $table->text('description');
+            $table->text('text');
+            $table->text('title');
+            $table->text('video_link');
+            $table->text('map');
+            $table->float('price');
+            $table->float('old_price');
         });
     }
 
