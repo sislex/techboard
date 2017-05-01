9d4e0357affc9bf10b03975eb64b5e082e02d866
Alex Rozhnov <alex.rozhnov@capsilon.com>
2017-05-01 13:19:28 +0300

    fixes

--- angular/src/app/services/catalog.service.ts
+++ angular/src/app/services/catalog.service.ts
@@ -9,13 +9,12 @@ export class CatalogService {
         public getAllCategories(idCategory: string) {
             return new Promise((resolve, reject) =>{
                 let headers = new Headers({
-                    'Content-Type': 'application/x-www-form-urlencoded',
-                    // 'X-CSRF-TOKEN': 'nLkKnC7B2moeaRChETcgEJqC6oN5qKaVM6YRi4t3',
+                    'Content-Type': 'application/x-www-form-urlencoded'
                 });
 
                 var params = new URLSearchParams();
-                params.set('id', 'idCategory');
-                
+                // params.set('id', 'idCategory');
+
                 this.http.post('http://techboard/api/catalog', params.toString(), { headers: headers }).subscribe((data: Response) => {
                     // this.menu = data.json();
                     resolve(data.json());
--- angular/src/app/services/goods.service.ts
+++ angular/src/app/services/goods.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Http, Response }   from '@angular/http';
+import {Http, Response, Headers, URLSearchParams} from '@angular/http';
 
 @Injectable()
 export class GoodsService {
@@ -9,14 +9,13 @@ export class GoodsService {
     public getAllGoods(idCategory:string){
         return new Promise((resolve, reject) =>{
             let headers = new Headers({
-                'Content-Type': 'application/x-www-form-urlencoded',
-                // 'X-CSRF-TOKEN': 'nLkKnC7B2moeaRChETcgEJqC6oN5qKaVM6YRi4t3',
+                'Content-Type': 'application/x-www-form-urlencoded'
             });
 
             var params = new URLSearchParams();
             params.set('catalog_id', idCategory);
 
-            this.http.post('http://techboard/api/goods', params.toString()).subscribe((data: Response) => {
+            this.http.post('http://techboard/api/goods', params.toString(), { headers: headers }).subscribe((data: Response) => {
                 // this.menu = data.json();
                 resolve(data.json());
             }, error => {
@@ -24,7 +23,7 @@ export class GoodsService {
                 reject(error);
             });
         });
-        
+
     }
     
     
