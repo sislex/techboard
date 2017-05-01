ff321e3acb8ab9ca282ba5b43db5385b04e747e6
Alex Rozhnov <alex.rozhnov@capsilon.com>
2017-05-01 13:50:39 +0300

    right route subscribe

--- angular/src/app/list/list.component.ts
+++ angular/src/app/list/list.component.ts
@@ -1,26 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import {GoodsService} from "../services/goods.service";
 import {ActivatedRoute, Router} from '@angular/router';
+import {Subscription} from 'rxjs/Subscription';
+
 
 @Component({
     selector: 'app-list',
     templateUrl: './list.component.html',
     styleUrls: ['./list.component.css']
 })
-export class ListComponent implements OnInit {
+export class ListComponent implements OnDestroy {
+    private subscription: Subscription;
     
     private idCategory;
     private goods;
-    constructor(private goodsService: GoodsService, private activatedRoute:ActivatedRoute, private router: Router) {}
+    constructor(private goodsService: GoodsService, private activatedRoute:ActivatedRoute, private router: Router) {
+        this.subscription = activatedRoute.params.subscribe(params => {
+            this.goodsService.getAllGoods(params['idCategory']).then((goods) => {
+                this.goods = goods;
+            });
+        });
+    }
 
-    ngOnInit() {
-        this.activatedRoute.params.forEach((value) => {
-            this.idCategory = value.idCategory;
-            // console.log("this.idCategory = " + this.idCategory);
-        });
-        this.goodsService.getAllGoods(this.idCategory).then((goods) => {
-            this.goods = goods;
-        });
+    ngOnDestroy(){
+        this.subscription.unsubscribe();
     }
 
 }
